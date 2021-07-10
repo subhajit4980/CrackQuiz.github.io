9929289d56ddee94de6563f95209d311c297ea3d
subhajit patra <subhajitpatra498@gmail.com>
2021-07-10 14:43:27 +0530

    add

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const quizData=[
-{   question: "What will a UPS be used for in a building?",
+{   question: "What will a UPS be used for in a building?*",
      a:" To provide power to essential equipment",
      b:" To monitor building electricity use",
      c:" To carry messages between departments",
      d:" To control lighting and power systems",
     correct:'a'
 },
-{   question: "The most common format for a home video recorder is VHS. VHS stands for...?",
+{   question: "The most common format for a home video recorder is VHS. VHS stands for...?*",
      a:" Video Home System",
      b:" Very high speed",
      c:" Video horizontal standard",
      d:" Voltage house standard",
     correct:'a'
 },
-{   question: "'OS' computer abbreviation usually means ?",
+{   question: "'OS' computer abbreviation usually means ?*",
      a:" Order of Significance",
      b:" Open Software",
      c:" Operating System",
      d:" Optical Sensor",
     correct:'c'
 },
-{   question: "Who developed Yahoo?",
+{   question: "Who developed Yahoo?*",
      a:"Dennis Ritchie & Ken Thompson",
      b:"David Filo & Jerry Yang",
      c:"Vint Cerf & Robert Kahn",
      d:"Steve Case & Jeff Bezos",
     correct:'b'
 },
-{   question: "What is part of a database that holds only one type of information?",
+{   question: "What is part of a database that holds only one type of information?*",
      a:" Report",
      b:" Field",
      c:" Record",
@@ -88,7 +88,23 @@ submitbtn.addEventListener('click',()=>
     else{
         // alert("you successfully finished all questions");
 
-        quiz.innerHTML=`<h2> Result:<br> You answered correctly at <b>${score}/${quizData.length}</b><br>Thanks for your response🎉🎊</h2>`;
+        // quiz.innerHTML=`<h2> Result:<br> You answered correctly at <b>${score}/${quizData.length}</b><br>Thanks for your response🎉🎊</h2>`;
+        if(score==5){
+            quiz.innerHTML=`<h2> Result:<br> You answered correctly at <b>${score}/${quizData.length}</b><br>Thanks for your response🎉🎊<br>You won gold🥇</h2>`;
+        
+        }
+        else if(score>=3 && score<=4){
+            quiz.innerHTML=`<h2> Result:<br> You answered correctly at <b>${score}/${quizData.length}</b><br>Thanks for your response🎉🎊<br>You won silver🥈</h2>`;
+       
+        }
+        else if(score>=1 && score<=2){
+            quiz.innerHTML=`<h2> Result:<br> You answered correctly at <b>${score}/${quizData.length}</b><br>Thanks for your response🎉🎊<br>You won bronze🥉</h2>`;
+       
+        }
+        else{
+            quiz.innerHTML=`<h2> Result:<br> You answered correctly at <b>${score}/${quizData.length}</b><br>Thanks for your response🎉🎊<br>Sorry you failed!❌</h2>`;
+       
+        }
     }
 }
 });
